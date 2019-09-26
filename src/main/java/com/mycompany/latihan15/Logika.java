961412c6aa1e42e6797e00f1d37fdc4715f98646
Tendy Hermansyah <55569231+TendyHemansyah@users.noreply.github.com>
2019-09-26 11:39:59 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihan15/Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan15;
+
+/**
+ *
+ * @author Tendy
+ * NAMA     : Tendy Hermansyah
+ * KELAS    : PBO2
+ * NIM      : 10118083
+ * Deskripsi Program : Logika
+ */
+public class Logika {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b ) = " + !(a && b));
+    }
+}
